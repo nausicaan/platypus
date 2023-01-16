b5f00b827f72ef6ab7bc033d2161dc453453bbbc
bstuike <byron.stuike@gov.bc.ca>
2023-01-16 11:24:33 -0800

    Add build version

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	w "github.com/nausicaan/wp-checker/workers"
 )
 
+var BuildVersion = "1.0.0"
+
 // Launch the program and execute according to the supplied flag
 func main() {
 	w.Plugin()
